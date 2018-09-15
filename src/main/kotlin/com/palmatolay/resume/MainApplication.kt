584b1bc45563a5f55c92b646ed8437ac97d2a6d5
Pál Matolay <matolaypal@gmail.com>
2018-09-15 14:17:00 +0200

    spring-security initialized (prepare for iframe)

--- src/main/kotlin/com/palmatolay/resume/MainApplication.kt
+++ src/main/kotlin/com/palmatolay/resume/MainApplication.kt
@@ -2,6 +2,10 @@ package com.palmatolay.resume
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
@@ -23,5 +27,16 @@ class MainController {
 //        model.addAttribute("user", User())
 //        th:text="${user.introduction}"
         return "index"
+    }
+}
+
+@EnableWebSecurity
+@Configuration
+class WebSecurityConfig : WebSecurityConfigurerAdapter() {
+
+    @Throws(Exception::class)
+    override fun configure(http: HttpSecurity) {
+        http.antMatcher("/**").authorizeRequests().anyRequest().permitAll()
+        http.headers().frameOptions().sameOrigin()
     }
 }
